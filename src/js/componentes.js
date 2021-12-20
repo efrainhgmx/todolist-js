f8d00739b811ae0c146ed2f3ecfb334e2c5244f4
Efrain Hernandez <efrainhg2050@gmail.com>
2021-12-19 19:47:14 -0600

    References hmtl

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,3 +1,19 @@
-export const crearTodoHtml = () => {
+//Referencias HTML
+
+const divTodoList = document.querySelector('.todo-list');
+
+
+export const crearTodoHtml = ( todo ) => {
+    
+    const htmlTodo = `
+    <li class="completed" data-id="abc">
+		<div class="view">
+			<input class="toggle" type="checkbox" checked>
+			<label>${ todo.tarea }</label>
+			<button class="destroy"></button>
+		</div>
+		<input class="edit" value="Create a TodoMVC template">
+	</li>`;
+
     
 }
