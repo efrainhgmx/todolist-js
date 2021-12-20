25f6c5def4c19d047d5b82188f40a525c52f1945
Efrain Hernandez <efrainhg2050@gmail.com>
2021-12-19 19:54:53 -0600

    Completed and check list

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
 import './styles.css'; 
 const todoList = new TodoList();
 const tarea = new Todo('Aprender JavaScript');
@@ -7,3 +8,5 @@ console.log(tarea);
 todoList.nuevoTodo( tarea );
 
 console.log( todoList);
+
+crearTodoHtml( tarea );
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -6,14 +6,19 @@ const divTodoList = document.querySelector('.todo-list');
 export const crearTodoHtml = ( todo ) => {
     
     const htmlTodo = `
-    <li class="completed" data-id="abc">
+    <li class="${(todo.completado) ? 'completed': ''}" data-id="${todo.id}">
 		<div class="view">
-			<input class="toggle" type="checkbox" checked>
+			<input class="toggle" type="checkbox" ${(todo.completado) ? 'cheked' : ''}>
 			<label>${ todo.tarea }</label>
 			<button class="destroy"></button>
 		</div>
 		<input class="edit" value="Create a TodoMVC template">
 	</li>`;
 
-    
+    const div = document.createElement('div');
+    div.innerHTML = htmlTodo;
+
+    divTodoList.append( div );
+
+    return div;
 }
