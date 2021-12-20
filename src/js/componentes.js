ab50d96234d2434d79344618360f714bba538cd8
Efrain Hernandez <efrainhg2050@gmail.com>
2021-12-19 19:42:38 -0600

    Created component for todo list

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,17 +1,3 @@
-import '../css/components.css';
-import webpackLogo from '../assets/img/webpack-logo.png';
-
-export const saludar = ( nombre ) => {
-    console.log("Creando el saludo");
-
-    const h1 =  document.createElement('h1');
-
-    h1.innerText = `Hola, ${nombre}`;
-
-    document.body.append(h1);
-
-    console.log(webpackLogo);
-    const img = document.createElement('img');
-    img.src = webpackLogo;
-    document.body.append( img );
+export const crearTodoHtml = () => {
+    
 }
