8135fdfba6f97227f6bf64504ca7839de71d1cc9
Efrain Hernandez <efrainhg2050@gmail.com>
2022-01-21 20:03:46 -0600

    Selected todo item function

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -8,6 +8,7 @@ const divTodoList = document.querySelector('.todo-list');
 const txtInput    = document.querySelector('.new-todo');
 const btnBorrar = document.querySelector('.clear-completed');
 const ulFiltros = document.querySelector('.filters');
+const anchorFiltros = document.querySelectorAll('.filtro')
 
 
 export const crearTodoHtml = ( todo ) => {
@@ -78,6 +79,9 @@ ulFiltros.addEventListener('click', (event) => {
 	const filtro = event.target.text;
 
 	if(!filtro) return;
+
+	anchorFiltros.forEach( elem => elem.classList.remove('selected'));
+	event.target.classList.add('selected');
 
 	for( const elemento of divTodoList.children ) {
 		elemento.classList.remove('hidden');
